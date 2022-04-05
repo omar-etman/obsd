fed21c5febae8707831950c0a41c338ca6dd079b
omar-etman <omartareketman@gmail.com>
2022-04-05 16:32:22 +0200

    build the navbar component & implement the dropdown functionality in laptop view

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,9 +1,39 @@
-import React from 'react'
+import { useState } from 'react'
+import { Link } from 'react-router-dom'
+import { Badge } from '@mui/material'
+import Cart from '../Cart/Cart'
+import BikeIcon from '../../assets/fastDelivery.png'
 import './Nav.css'
 
-function Nav() {
+function Nav({dropDown3, toggleDropDown3}) {
+
   return (
-    <div>Nav</div>
+    <div className='nav-container'>
+        <Link to = "/">
+            <div className='nav-brand'>
+                <span className='brand-name'>OBSD</span>
+                <img src='' alt='' className='brand-logo'/>
+            </div>
+        </Link>
+        <ul className='nav-buttons'>
+            <li className='buttons-btn'>Menu</li>
+            <li className='buttons-btn'>Most Popular</li>
+            <li className='buttons-bikeBtn' onClick={toggleDropDown3}>
+                <Badge badgeContent={4} color="primary" >
+                    <img src={BikeIcon} alt='bikeIcon' className='bikebtn-icon'/>
+                </Badge>
+            </li>
+            {
+                dropDown3 === true ?
+
+                <div className='nav-dropDown-3'>
+                    <Cart/>
+                </div>
+
+                : null
+            }
+        </ul>
+    </div>
   )
 }
 
---
+++ src/pages/App.js
@@ -0,0 +1,41 @@
+import Nav from '../components/Nav/Nav';
+import Home from './HomePage/Home';
+import Form from './Form/Form';
+import DashBoard from './DashBoard/DashBoard';
+import { useState } from 'react'
+
+import { BrowserRouter, BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import './App.css';
+import { Suspense } from 'react';
+
+function App() {
+
+  const [dropDown3, viewDropDown3] = useState(false);
+
+  const toggleDropDown3 = (e) => {
+      viewDropDown3(!dropDown3)
+  }
+
+  return (
+    <div className="App">
+      <BrowserRouter>
+          <header className="App-header">
+            <Nav
+              dropDown3 = {dropDown3}
+              toggleDropDown3 = {toggleDropDown3}
+            />
+          </header>
+          <Routes>
+            <Route path = "/" element={<Home/>}/>
+            <Route path = "/form" element={<Form/>}/>
+            <Route path = "/dashboard" element={<DashBoard/>}/>
+          </Routes>
+          <footer className='App-footer'>
+            footer
+          </footer>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
