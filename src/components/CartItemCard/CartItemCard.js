06ec1d1f8267aecac39200366c006219a49f89c8
omar-etman <omartareketman@gmail.com>
2022-04-07 04:00:12 +0200

    Setup the checkout form page's HTML

---
+++ src/components/CartItemCard/CartItemCard.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import './CartItemCard.css'
+function CartItemCard() {
+  return (
+    <div>CartItemCard</div>
+  )
+}
+
+export default CartItemCard
--- src/pages/Form/Form.js
+++ src/pages/Form/Form.js
@@ -1,9 +1,87 @@
-import React from 'react'
+import { useState } from 'react'
+import CartItemCard from '../../components/CartItemCard/CartItemCard'
 import './Form.css'
 
 function Form() {
+
+  //recieves the cart array
+  //sends orders to dashboard
+
+  const [name, setName] = useState("");
+  const [mobile, setMobile] = useState("");
+  const [address, setAddress] = useState("");
+  const [city, setCity] = useState("");
+
+  const updateName = (e) => {
+      setName(e.target.value)
+  }
+
+  const updateMobile = (e) => {
+      setMobile(e.target.value)
+  }
+
+  const updateAddress = (e) => {
+      setAddress(e.target.value)
+  }
+
+  const updateCity = (e) => {
+      setCity(e.target.value)
+  }
+
   return (
-    <div>Form</div>
+    <div className='form-container'>
+        <div className='form-orders'>
+            <ul className='ordered-cartItems-list'>
+                <li><CartItemCard/></li>
+                <li><CartItemCard/></li>
+                <li><CartItemCard/></li>
+                <li><CartItemCard/></li>
+            </ul>
+            <p className='orders-subTotal'>Subtotal: LE subtotal</p>
+        </div>
+        <form>
+            <label htmlFor="name">
+                Name
+                <input
+                    id="name"
+                    onChange={updateName}
+                    value={name}
+                    placeholder="i.e John Smith"
+                />
+            </label>
+            <label htmlFor="mobile">
+                Mobile
+                <input
+                    id="mobile"
+                    onChange={updateMobile}
+                    value={mobile}
+                    placeholder="+201234567890"
+                />
+            </label>
+            <label htmlFor="address">
+                Address
+                <input
+                    id="name"
+                    onChange={updateAddress}
+                    value={address}
+                    placeholder="residence number, street, area."
+                />
+            </label>
+            <label htmlFor="city">
+                City
+                <input
+                    id="city"
+                    onChange={updateCity}
+                    value={city}
+                    placeholder="Cairo, Egypt"
+                />
+            </label>
+            <div className='form-buttons'>
+                <button className='buttons-orderNow'>Order Now</button>
+                <button className='buttons-cancel'>cancel</button>
+            </div>
+        </form>
+    </div>
   )
 }
 
